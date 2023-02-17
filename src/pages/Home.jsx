62510d824d64aead445fcdfee466b5d7df4f3936
devLda <leducanh.mywork@gmail.com>
2023-02-17 17:36:26 +0700

    Update 17/02

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,39 @@ import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero.png";
 import '../styles/hero-section.css'
 
+import Category from "../components/UI/category/Category.jsx";
+import "../styles/home.css"
+
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+const featureData = [
+  {
+    title: "Quick Delivery",
+    imgUrl: featureImg01,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+
+  {
+    title: "Super Dine In",
+    imgUrl: featureImg02,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+  {
+    title: "Easy Pick Up",
+    imgUrl: featureImg03,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+];
+
 const Home = () => {
   return (
     <Helmet title="Home">
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6">
+            <Col>
               <div className="hero__content">
                 <h5 className="mb-3">Easy way to make an order</h5>
                 <h1 className="mb-4 hero__title">
@@ -21,22 +47,78 @@ const Home = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Quasi, excepturi ab commodi
                 </p>
-                <div className="hero__btns d-flex align-items-center gap-5 mt-4">
-                  <button className="order__btn d-flex align-items-center justify-content-between">
-                    Order now <i class="ri-arrow-right-s-line"></i>
+                <div className="hero__btns mt-4">
+                  <button className="order__btn d-flex align-items-center mb-4">
+                    <span>Order Now</span>  <i class="ri-arrow-right-s-line"></i>
                   </button>
 
                   <button className="all__foods-btn">
                     <Link to="/foods">See all foods</Link>
                   </button>
                 </div>
+
+                <div className=" hero__service  d-flex align-items-center gap-5 mt-5 ">
+                  <p className=" d-flex align-items-center gap-2 ">
+                    <span className="shipping__icon">
+                      <i class="ri-car-line"></i>
+                    </span>{" "}
+                    No shipping charge
+                  </p>
+
+                  <p className=" d-flex align-items-center gap-2 ">
+                    <span className="shipping__icon">
+                      <i class="ri-shield-check-line"></i>
+                    </span>{" "}
+                    100% secure checkout
+                  </p>
+                </div>
               </div>
             </Col>
-            <Col lg="6" md="6">
+            <Col>
               <div className="hero__img">
-                <img src={heroImg} alt="hero-img" className="w-100" />
+                <img src={heroImg} alt="hero-img" />
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt-0">
+        <Category />
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h5 className="feature__subtitle mb-4">What we serve</h5>
+              <h2 className="feature__title">Just sit back at home</h2>
+              <h2 className="feature__title">
+                we will <span>take care</span>
+              </h2>
+              <p className="mb-1 mt-4 feature__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                officiis?
+              </p>
+              <p className="feature__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, eius.amet consectetur adipisicing elit{" "}
+              </p>
+            </Col>
+
+            {featureData.map((item, index) => (
+              <Col lg="4" md="6" sm="6" key={index} className="mt-5">
+                <div className="feature__item text-center px-5 py-3">
+                  <img
+                    src={item.imgUrl}
+                    alt="feature-img"
+                    className="w-25 mb-3"
+                  />
+                  <h5 className=" fw-bold mb-3">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
